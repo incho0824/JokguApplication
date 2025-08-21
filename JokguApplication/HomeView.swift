8d4b01c1f7c4ed70861063f0efeccc5e862d1416
incho0824 <160176412+incho0824@users.noreply.github.com>
2025-08-20 22:51:03 -0400

    Separate HomeView into its own file

---
+++ JokguApplication/HomeView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Atlanta Jokgu Association")
+            .font(.title)
+            .padding()
+    }
+}
+
+#Preview {
+    HomeView()
+}
