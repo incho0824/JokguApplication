e95ee9225d1e5a412ba41fee2541c9e657998cf7
incho0824 <incho0824@gmail.com>
2025-08-20 22:23:57 -0400

    Initial Commit

---
+++ JokguApplication/JokguApplicationApp.swift
@@ -0,0 +1,32 @@
+//
+//  JokguApplicationApp.swift
+//  JokguApplication
+//
+//  Created by In Cho on 8/20/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct JokguApplicationApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ JokguApplicationTests/JokguApplicationTests.swift
@@ -0,0 +1,17 @@
+//
+//  JokguApplicationTests.swift
+//  JokguApplicationTests
+//
+//  Created by In Cho on 8/20/25.
+//
+
+import Testing
+@testable import JokguApplication
+
+struct JokguApplicationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
