f6c766951147b27a3885fdb515ef12faeb158b55
incho0824 <160176412+incho0824@users.noreply.github.com>
2025-08-25 19:15:29 -0400

    Hide guest members in reorder view

--- JokguApplication/PostHomeViews/ProUsers/ReorderMembersView.swift
+++ JokguApplication/PostHomeViews/ProUsers/ReorderMembersView.swift
@@ -3,6 +3,7 @@ import SwiftUI
 struct ReorderMembersView: View {
     @Environment(\.dismiss) var dismiss
     @State private var members: [Member] = []
+    @State private var guestMembers: [Member] = []
 
     var body: some View {
         List {
@@ -21,14 +22,24 @@ struct ReorderMembersView: View {
             }
         }
         .onAppear {
-            members = DatabaseManager.shared.fetchMembers()
+            let allMembers = DatabaseManager.shared.fetchMembers()
+            members = allMembers.filter { $0.guest != 0 }
+            guestMembers = allMembers.filter { $0.guest == 0 }
+            updateOrderIndices()
         }
     }
 
     private func move(from source: IndexSet, to destination: Int) {
         members.move(fromOffsets: source, toOffset: destination)
+        updateOrderIndices()
+    }
+
+    private func updateOrderIndices() {
         for (index, member) in members.enumerated() {
             _ = DatabaseManager.shared.updateOrder(id: member.id, order: index)
+        }
+        for (offset, member) in guestMembers.enumerated() {
+            _ = DatabaseManager.shared.updateOrder(id: member.id, order: members.count + offset)
         }
     }
 }
